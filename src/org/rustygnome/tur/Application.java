35c05d3dee702382069b6c0e305a300a5478427d
Christian Reiner <pow@christian-reiner.info>
2019-12-11 16:41:25 +0100

    added gradle build support

--- src/org/rustygnome/tur/Application.java
+++ src/org/rustygnome/tur/Application.java
@@ -1,11 +1,7 @@
 package org.rustygnome.tur;
 
-import org.apache.commons.codec.DecoderException;
 import org.rustygnome.tur.artefact.Controller;
 import org.rustygnome.tur.artefact.Logger;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 public class Application {
 
